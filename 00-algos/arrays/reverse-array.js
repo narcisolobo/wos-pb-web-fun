ff2664ce716aa647da5eb2f2c435152dfd4fff72
narcisolobo <narcisolobo@gmail.com>
2024-06-26 07:32:31 -0700

    w1d3 algo

---
+++ 00-algos/arrays/reverse-array.js
@@ -0,0 +1,26 @@
+// JSDoc
+
+/**
+ * This function returns true if the given
+ * word is a palindrome, and false if it is
+ * not.
+ * @param {string} word
+ */
+function isPalindrome(word) {
+  // use a while loop
+  let start = 0;
+  let end = word.length - 1;
+  while (start < end) {
+    // do something
+  }
+}
+
+/**
+ * Define a function that, given an array,
+ * reverses the order of the items in that array
+ * in-place, returning that same array.
+ * @param {any[]} arr
+ */
+function reverseArr(arr) {
+  // your code here
+}

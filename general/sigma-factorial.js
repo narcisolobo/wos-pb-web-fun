9542e74e395fd3d8fa69654c04a2b223e002db5b
narcisolobo <narcisolobo@gmail.com>
2024-06-27 07:05:37 -0700

    added algo w1-d4

---
+++ general/sigma-factorial.js
@@ -0,0 +1,38 @@
+/*
+  Sigma
+  Implement a function ​sigma(num)​ that, given a number,
+  returns the sum of all positive integers from 1 up to num (inclusive).
+
+  Ex.:
+  ​sigma(3) = 6 (or 1 + 2 + 3)
+  ​sigma(5)​ = 15 (or 1 + 2 + 3 + 4 + 5)
+*/
+
+/**
+ *
+ * @param {number} num
+ * @returns {number}
+ */
+function sigma(num) {
+  // your code
+}
+
+/*
+  Factorial
+  Write a function ​factorial(num)​ that, given a number,
+  returns the product (multiplication) of all positive
+  integers from 1 up to num (inclusive).
+  
+  For example:
+  ​factorial(3)​ = 6 (or 1 * 2 * 3)
+  factorial(5)​ = 120 (or 1 * 2 * 3 * 4 * 5)
+*/
+
+/**
+ *
+ * @param {number} num
+ * @returns {number}
+ */
+function factorial(num) {
+  // your code
+}

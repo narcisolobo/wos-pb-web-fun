3b431d2910e59472b6fe2c94935b6f3cceb8a396
narcisolobo <narcisolobo@gmail.com>
2024-06-28 07:03:30 -0700

    w1d5 algo

---
+++ 01-algos/general/sum-to-one.js
@@ -0,0 +1,23 @@
+/* 
+  Sum To One Digit
+  Implement a function ​sumToOne(num)​ that, given
+  a number, sums that number’s digits repeatedly
+  until the sum is only one digit. Return that
+  final one digit result.
+
+  Ex.
+  sumToOne(5) // 5
+  sumToOne(20) // 2
+  sumToOne(849) // 3
+*/
+
+/**
+ * This function takes in an integer as input and
+ * sums that number’s digits repeatedly until the
+ * sum is only one digit.
+ * @param {number} num
+ * @returns {number}
+ */
+function sumToOne(num) {
+  // your code here
+}

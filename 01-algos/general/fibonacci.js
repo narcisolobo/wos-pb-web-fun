7bbb9a88e8d89b8b87706476b99a695af9cc07c1
narcisolobo <narcisolobo@gmail.com>
2024-07-01 07:15:48 -0700

    added fib algo

---
+++ 01-algos/general/fibonacci.js
@@ -0,0 +1,29 @@
+/* 
+  Fibonacci
+  Implement the Fibonacci function, a famous mathematical
+  equation that generates a numerical sequence such that
+  each number is the sum of the previous two.
+  
+  The Fibonacci numbers at index 0 and 1, coincidentally,
+  have values of 0 and 1. Your function should accept an
+  argument `idx` and return the Fibonacci number at that
+  index.
+
+  Examples:
+  ​fibonacci(2)​ = 1
+  fibonacci(3)​ = 2
+  ​fibonacci(4)​ = 3
+  ​fibonacci(5)​ = 5
+
+  https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+  https://www.mathsisfun.com/numbers/golden-ratio.html
+
+  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+   0  1  2  3  4  5  6  7   8   9
+*/
+
+function fibonacci(idx) {
+  const fib = [0, 1];
+  // loop starts at index 2
+  // inside the loop, push to fib array
+}

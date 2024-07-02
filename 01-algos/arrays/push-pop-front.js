09f90fe685454a916901d0c3da2ff605f1f97a0e
narcisolobo <narcisolobo@gmail.com>
2024-07-02 06:58:07 -0700

    added w2d2 algo setup

---
+++ 01-algos/arrays/push-pop-front.js
@@ -0,0 +1,35 @@
+/* 
+  PushFront
+  Given an array and an additional value, ​insert this
+  value ​at the beginning of the array. Do this without
+  using any built-in array methods. Return the modified
+  array.
+*/
+
+/**
+ * pushFront inserts the given value at index 0 of the
+ * given array.
+ * @param {any[]} arr
+ * @param {any} val
+ * @returns {any[]}
+ */
+function pushFront(arr, val) {
+  // your code here
+}
+
+/* 
+  PopFront
+  Given an array, ​remove and return the value ​at the
+  beginning of the array. Do this without using any
+  built-in array methods except ​pop()​.
+*/
+
+/**
+ * popFront removes and returns the value at index 0
+ * of the given array.
+ * @param {any[]} arr
+ * @returns {any}
+ */
+function popFront(arr) {
+  // your code here
+}

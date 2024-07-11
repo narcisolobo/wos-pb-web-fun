f6282a1bff5d7773f43b2e09bff4240bf13acc8b
narcisolobo <narcisolobo@gmail.com>
2024-07-11 07:01:04 -0700

    algo 7/11

---
+++ 00-algos/strings/decode-string.js
@@ -0,0 +1,28 @@
+/* 
+  String Decode  
+*/
+
+const str1 = 'a3b2c1d3';
+const expected1 = 'aaabbcddd';
+
+const str2 = 'a3b2c12d10';
+const expected2 = 'aaabbccccccccccccdddddddddd';
+
+/**
+ * Decodes the given string by duplicating each character by the following int
+ * amount if there is an int after the character.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str An encoded string with characters that may have an int
+ *    after indicating how many times the character occurs.
+ * @returns {string} The given str decoded / expanded.
+ */
+function decodeStr(str) {
+  // your code here
+}
+
+const result1 = decodeStr(str1);
+console.log(`${result1} should equal ${expected1}`);
+
+const result2 = decodeStr(str2);
+console.log(`${result2} should equal ${expected2}`);
---
+++ 00-algos/strings/encode-string.js
@@ -0,0 +1,50 @@
+/* 
+  Given in an alumni interview in 2021.
+
+  String Encode
+
+  You are given a string that may contain sequences of consecutive characters.
+  Create a function to shorten a string by including the character,
+  then the number of times it appears. 
+  
+  
+  If final result is not shorter (such as "bb" => "b2" ),
+  return the original string.
+  */
+
+const str1 = 'aaaabbcddd';
+const expected1 = 'a4b2c1d3';
+
+const str2 = '';
+const expected2 = '';
+
+const str3 = 'a';
+const expected3 = 'a';
+
+const str4 = 'bbcc';
+const expected4 = 'bbcc';
+
+/**
+ * Encodes the given string such that duplicate characters appear once followed
+ * by a number representing how many times the char occurs. Only encode strings
+ * when the result yields a shorter length.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str The string to encode.
+ * @returns {string} The given string encoded.
+ */
+function encodeStr(str) {
+  // your code here
+}
+
+const result1 = encodeStr(str1);
+console.log(`${result1} should equal ${expected1}`);
+
+const result2 = encodeStr(str2);
+console.log(`${result2} should equal ${expected2}`);
+
+const result3 = encodeStr(str3);
+console.log(`${result3} should equal ${expected3}`);
+
+const result4 = encodeStr(str4);
+console.log(`${result4} should equal ${expected4}`);

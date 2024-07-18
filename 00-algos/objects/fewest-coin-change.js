c61b9292b6acacc879e07c5d3bdcbcdd1c8396ce
narcisolobo <narcisolobo@gmail.com>
2024-07-18 08:49:40 -0700

    w4d4 algo solution

--- 00-algos/objects/fewest-coin-change.js
+++ 00-algos/objects/fewest-coin-change.js
@@ -16,6 +16,9 @@ const expected3 = { nickel: 1, penny: 4 };
 const cents4 = 99;
 const expected4 = { quarter: 3, dime: 2, penny: 4 };
 
+const cents5 = 41;
+const expected5 = { quarter: 1, dime: 1, nickel: 1, penny: 1 };
+
 /**
  * Calculates the fewest coins of the standard American denominations needed
  *    to reach the given cents amount.
@@ -28,5 +31,35 @@ const expected4 = { quarter: 3, dime: 2, penny: 4 };
  *    needed.
  */
 function fewestCoinChange(cents) {
-  // your code here
+  const coins = {};
+
+  if (cents >= 25) {
+    const quarters = Math.floor(cents / 25);
+    coins.quarter = quarters;
+    cents -= quarters * 25;
+  }
+
+  if (cents >= 10) {
+    const dimes = Math.floor(cents / 10);
+    coins.dime = dimes;
+    cents -= dimes * 10;
+  }
+
+  if (cents >= 5) {
+    const nickels = Math.floor(cents / 5);
+    coins.nickel = nickels;
+    cents -= nickels * 5;
+  }
+
+  if (cents >= 1) {
+    coins.penny = cents;
+  }
+
+  return coins;
 }
+
+console.log(fewestCoinChange(cents1));
+console.log(fewestCoinChange(cents2));
+console.log(fewestCoinChange(cents3));
+console.log(fewestCoinChange(cents4));
+console.log(fewestCoinChange(cents5));
